2983f0933293d6a13ce362538a5cde9d97b5a8f5
Raghib Lakdawala <raaghib12@gmail.com>
2022-06-08 09:24:11 +0100

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-function getRandomInt(max) {
-    return Math.floor(Math.random()*3)
+function computerPlay() {
+    let computerSelection = '';
+    let randomNum = Math.floor(Math.random()*3)
+
+    if (randomNum === 0) { 
+        computerSelection = "Rock";
+    }
+
+    else if (randomNum === 1) {
+        computerSelection = "Paper";
+    }
+    
+    else {
+        computerSelection = "Scissors"
+    }
+
+    return(computerSelection)
 }
 
-console.log(getRandomInt())
+function playerSelection(string) {
+    let playerSelection = prompt("Please choose Rock, Paper or Scissors");
+    return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+}
+
+console.log(playerSelection())
