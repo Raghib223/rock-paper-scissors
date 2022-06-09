5fa897c09f5e00ecd0dc61a0740e70dd01373d11
Raghib Lakdawala <raaghib12@gmail.com>
2022-06-09 09:02:21 +0100

    index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function computerPlay() {
-    let computerSelection = '';
+function computerSelection() {
+    let computerSelection = ""
     let randomNum = Math.floor(Math.random()*3)
 
     if (randomNum === 0) { 
@@ -14,7 +14,7 @@ function computerPlay() {
         computerSelection = "Scissors"
     }
 
-    return(computerSelection)
+    return(computerSelection);
 }
 
 function playerSelection(string) {
@@ -22,71 +22,3 @@ function playerSelection(string) {
     return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
 }
 
-function playRound(playerSelection, computerSelection) {
-    let winner = ''
-
-    if (playerSelection === computerSelection ) {
-        return "It's a tie";
-    }
-
-    else if (playerSelection === "Rock" && computerSelection === "Paper") {
-        winner = "You Loser! Paper beats Rock"
-    }
-
-    else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        winner = "You Win! Rock beats Scissors"
-    }
-
-    else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        winner = "You Win! Paper beats Rock"
-    }
-
-    else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-        winner = "You Loser! Scissors beats Paper"
-    }
-
-    else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        winner = "You Loser! Scissors beats Paper"
-    }
-
-    else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-        winner = "You Win! Paper beats Rock"
-    }
-
-    return winner
-}
-
-function game() {
-    playRound(playerSelection, computerSelection)
-    let roundResult = "";
-    let playerScore = 0;
-    let computerScore = 0;
-    let playerTurn = "";
-
-    for (let i = 0; i < 5; i++) {
-        computerTurn = computerPlay();
-        playerTurn = playerSelection();
-        roundResult = playRound(playerSelection, computerSelection);
-        if (roundResult.includes("You Win")) {
-            playerScore+=1;
-
-        }
-            else if (roundResult.includes("You Loser")) {
-            computerScore+=1;
-            console.log(roundResult + "The Score is" + playerScore + "to" + computerScore)
-        }
-    }
-
-    if (playerScore > computerScore) {
-        console.log("You won the game!" + playerScore + "to" + computerScore)
-    }
-    else if (playerScore < computerScore) {
-        console.log("You lost the game" + playerScore + "to" + computerScore)
-    }
-    else {
-        console.log("It's a tie!")
-    }
-
-}
-
-game()
