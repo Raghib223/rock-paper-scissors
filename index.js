03a79cbb48ba34fc1248991e1b3e3952fe985945
Raghib Lakdawala <raaghib12@gmail.com>
2022-06-09 08:01:18 +0100

    index.js

--- index.js
+++ index.js
@@ -22,4 +22,37 @@ function playerSelection(string) {
     return playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
 }
 
-console.log(playerSelection())
+function playRound(playerSelection, computerSelection) {
+    let winner = ''
+
+    if (playerSelection === computerSelection ) {
+        return "It's a tie";
+    }
+
+    else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        winner = "You loser! Paper beats Rock"
+    }
+
+    else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        winner = "You Win! Rock beats Scissors"
+    }
+
+    else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        winner = "You Win! Paper beats Rock"
+    }
+
+    else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        winner = "You Loser! Scissors beats Paper"
+    }
+
+    else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        winner = "You Loser! Scissors beats Paper"
+    }
+
+    else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        winner = "You Win! Paper beats Rock"
+    }
+
+    return winner
+}
+
